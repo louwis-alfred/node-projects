d65093823b082d5ec8f9f36b6b8d9390f84ce2c2
Somnus <nabayan.louwisalfred.08142002@gmail.com>
2025-06-04 12:53:28 +0800

    Upload express project

--- express/src/server.ts
+++ express/src/server.ts
@@ -1,20 +1,19 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRouter from './routes/auth/'
+
 dotenv.config();
 
 const app = express();
-app.use(cors());
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use('/api/auth', authRouter)
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port http://localhost:${process.env.PORT}`);
-});
+    console.log(`Server running on port http://localhost:${process.env.PORT}`)
+})
