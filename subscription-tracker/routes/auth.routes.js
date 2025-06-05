754002fe91af70ab9169e2f59009fd78e1874743
Somnus <nabayan.louwisalfred.08142002@gmail.com>
2025-06-06 00:20:23 +0800

    Remove subscription-tracker from repository

--- subscription-tracker/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const authRouter = Router();
-
-authRouter.post("/sign-up", (req, res) => res.send("Sign up"));
-authRouter.post("/sign-in", (req, res) => res.send("Sign in"));
-authRouter.get("/sign-out", (req, res) => res.send("Sign out"));
-
-export default authRouter;
